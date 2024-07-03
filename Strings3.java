d0a4f2047e1da694b578235959cb5ed76219597c
Saugandhika3021 <21up1a05d5.vmtw@gmail.com>
2024-07-03 13:46:45 +0530

    Add files via upload

---
+++ Strings3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Strings3
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+String str="java is simple language";
+System.out.println(str.substring(12));
+System.out.println(str.substring(8,18));
+}
+}
+
